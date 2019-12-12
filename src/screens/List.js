98ee968c832607a504c7b67522d38343deaa478d
Gibson Ainge <gainge@gmail.com>
2019-12-12 11:58:19 -0800

    add stub list component

---
+++ src/screens/List.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+import { connect } from 'react-redux'
+
+export default class List extends Component {
+  render() {
+    return (
+      <View>
+        <Text> textInComponent </Text>
+      </View>
+    )
+  }
+}
