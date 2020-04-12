7ef09f0abb7f3de91e845bd65231b77583d65b77
Gibson Ainge <gainge@gmail.com>
2020-04-12 12:38:03 -0600

    add separate screen for list overview

---
+++ src/screens/ListOverview.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default class ListOverview extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Lots of lists! </Text>
+      </View>
+    )
+  }
+}
