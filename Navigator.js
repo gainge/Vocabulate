ac561c46e698bd1d15f51dd970fcf87cb55e38c3
Gibson Ainge <gainge@gmail.com>
2020-04-12 13:02:40 -0600

    implement early version of new navigation

--- Navigator.js
+++ Navigator.js
@@ -2,38 +2,121 @@ import React, { Component } from 'react'
 
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 
 import AppStyles from './src/styles/AppStyles'
 
 import Home from './src/screens/Home'
 import List from './src/screens/List'
+import ListOverview from './src/screens/ListOverview'
+import Settings from './src/screens/Settings'
+import Study from './src/screens/Study'
+import StudyConfig from './src/screens/StudyConfig'
 
 
-const AppNavigator = createStackNavigator(
+const getTabIcon = (iconName, color=AppStyles.color.primaryLightUI) => {
+  return (
+    <Icon name={iconName} size={AppStyles.UI.tabIconSize} color={color} />
+  )
+}
+
+
+const defaultStackNavigationOptions = {
+  defaultNavigationOptions: {
+    headerStyle: {
+      backgroundColor: AppStyles.color.primary,
+      borderBottomWidth: 0,
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+      fontSize: 30,
+      color: AppStyles.color.textLight,
+    },
+  },
+  headerLayoutPreset: 'center'
+};
+
+
+const StudyStack = createStackNavigator(
   {
     Home: Home,
-    List: List,
+    StudyConfig: StudyConfig,
+    Study: Study,
   },
   {
     initialRouteName: 'Home',
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: AppStyles.color.primary,
-        borderBottomWidth: 0,
-      },
-      headerTintColor: '#fff',
-      headerTitleStyle: {
-        fontWeight: 'bold',
-        fontSize: 30,
-        color: AppStyles.color.textLight,
-      },
-    },
-    headerLayoutPreset: 'center'
+    ...defaultStackNavigationOptions
   }
 );
 
-const AppContainer = createAppContainer(AppNavigator);
+const ListStack = createStackNavigator(
+  {
+    ListOverview: ListOverview,
+    List: List,
+  },
+  {
+    initialRouteName: 'ListOverview',
+    ...defaultStackNavigationOptions
+  }
+)
+
+const SettingsStack = createStackNavigator(
+  {
+    Settings: Settings,
+  },
+  {
+    initialRouteName: 'Settings',
+    ...defaultStackNavigationOptions
+  }
+)
+
+
+const TabNavigator = createBottomTabNavigator(
+  {
+    Home: {
+      screen: StudyStack,
+      navigationOptions: {
+        tabBarLabel: 'Home',
+        tabBarIcon: ({tintColor}) => getTabIcon('home', tintColor),
+      },
+    },
+    Lists: {
+      screen: ListStack,
+      navigationOptions: {
+        tabBarLabel: 'Lists',
+        tabBarIcon: ({tintColor}) => getTabIcon('list-ul', tintColor),
+      },
+    },
+    Settings: {
+      screen: SettingsStack,
+      navigationOptions: {
+        tabBarLabel: 'Settings',
+        tabBarIcon: ({tintColor}) => getTabIcon('cog', tintColor),
+      },
+    },
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: AppStyles.color.primaryLightUI,
+      activeBackgroundColor: AppStyles.color.activeTab,
+      inactiveTintColor: 'gray',
+      labelStyle: {
+        fontSize: AppStyles.fontSize.tabLabel,
+      },
+      style: {
+
+      },
+      tabStyle: {
+        paddingVertical: 3,
+      }
+    },
+  }
+);
+
+const AppContainer = createAppContainer(TabNavigator);
 
 class Navigator extends Component {
   render() {
