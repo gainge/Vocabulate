a12ca5ca0c77bf9e517331ca6353974e968b9590
Gibson Ainge <gainge@gmail.com>
2020-04-13 14:41:39 -0600

    update test for topic reducer for new architecture

--- __tests__/Reducer-test-topic.js
+++ __tests__/Reducer-test-topic.js
@@ -1,16 +1,12 @@
-import lingualApp from "../src/store/Reducers";
 import * as actions from '../src/store/Actions';
+import topicsReducer from '../src/store/reducers/Topics'
 
 describe('Topics Reducer', () => {
   it('should return the initial state', () => {
-    expect(lingualApp(undefined, {})).toEqual({
-      lists: [],
-      modalShown: false,
-      topics: []
-    })
+    expect(topicsReducer(undefined, {})).toEqual(
+      []
+    )
   })
-
-  
 })
 
 
