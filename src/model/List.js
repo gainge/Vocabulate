10ff7dfb1264da79d2bef0e2bf00418410e8c58f
Gibson Ainge <gainge@gmail.com>
2020-04-14 06:18:47 -0600

    add optional deserialization to list creation

--- src/model/List.js
+++ src/model/List.js
@@ -2,19 +2,25 @@ import Item from './Item'
 
 
 export default class List {
-  constructor(id, title, creationDate, items = []) {
+  constructor(id, title, creationDate, items = [], itemDeserialization = undefined) {
     this.id = id;
     this.title = title;
     this.creationDate = creationDate;
 
-    // Initialize the items from json data
-    this.items = items;
+    // Initialize the items from json if applicable
+    if (itemDeserialization) {
+      this.items = [];
+
+      items.forEach((item) => this.items.push(itemDeserialization(item)));
+    } else {
+      this.items = items;
+    }
 
     // Now we done?
   }
 
-  static deserialize(json) {
-    return new List(json.id, json.title, json.creationDate, json.items);
+  static deserialize(json, itemDeserialization = undefined) {
+    return new List(json.id, json.title, json.creationDate, json.items, itemDeserialization);
   }
 }
 
