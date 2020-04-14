d0160d5d95f9a4ade674fab8d45c9c1f064dbbbb
Gibson Ainge <gainge@gmail.com>
2020-04-14 06:19:04 -0600

    add new demo lists to data loader

--- src/util/DataLoader.js
+++ src/util/DataLoader.js
@@ -3,14 +3,33 @@
 // Eventually this will be replaced with some kind of async storage solution or something
 
 import List from './../model/List'
+import Topic from './../model/Topic'
+import Tool from './../model/Tool'
+import Vocab from './../model/Vocab'
 
 // Read json string from file
 const jsonData = require('../../assets/data/demoList.json');
 
+const demoTopicsJson = require('../../assets/data/demoTopics.json');
+const demoToolsJson = require('../../assets/data/demoTools.json');
+const demoVocabJson = require('../../assets/data/demoVocab.json');
+
 const appData = List.deserialize(jsonData);
 
+const demoTopics = List.deserialize(demoTopicsJson, Topic.deserialize);
+const demoTools = List.deserialize(demoToolsJson, Tool.deserialize);
+const demoVocab = List.deserialize(demoVocabJson, Vocab.deserialize);
+
+console.log(demoTopics);
+console.log(demoTools);
+console.log(demoVocab);
+
+
 export {
-  appData
+  appData,
+  demoTopics,
+  demoTools,
+  demoVocab,
 };
 
 
